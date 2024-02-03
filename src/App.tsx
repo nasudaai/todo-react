90f383b4bfceab1f3a85dc84484681c50563b94f
nasuda <nasuda908@gmail.com>
2024-02-03 09:21:42 +0900

    add function 'toggle' 'delete'

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { nanoid } from 'nanoid'
 import { useState } from 'react'
 import Form from './components/Form'
 import FilterButton from './components/FilterButton'
@@ -6,18 +7,36 @@ import Todo from './components/Todo'
 
 
 function App(props) {
-  const [tasks, setTasks] = useState(props.tasks)
-  console.log(props.tasks)
 
   //
   function addTask(name) {
-    const newTask = { id: 'id', name, completed: false}
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false}
     setTasks([...tasks, newTask])
-    alert(name)
+    console.log(newTask.id)
   }
 
 
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+
+      if (id === task.id) {
+        return { ...task, completed: !task.completed }
+      }
+      return task;
+    })
+    setTasks(updatedTasks)
+    console.log(tasks[id])
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id)
+    setTasks(remainingTasks)
+    console.log(id, remainingTasks)
+  }
+
   //map 
+
+  const [tasks, setTasks] = useState(props.tasks)
   const taskList = tasks?.map((task) => {
     return (
     <Todo 
@@ -25,9 +44,14 @@ function App(props) {
       name={task.name} 
       completed={task.completed} 
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
     />
     )
   })
+
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task'
+  const headingText = `${taskList.length} ${tasksNoun} remaining`
 
   return (
     <div className="todoapp stack-large">
@@ -39,7 +63,7 @@ function App(props) {
         <FilterButton />
         <FilterButton />     
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
